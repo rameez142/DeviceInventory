a89b1581c8e9edabd02ee9c38cb68e3a73b938f4
Rameez <rameez.rameez14@gmail.com>
2018-10-24 13:34:07 +0300

    s

--- src/QuickApp/Startup.cs
+++ src/QuickApp/Startup.cs
@@ -193,8 +193,7 @@ namespace AssetManagement
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
+               
 
                 spa.Options.SourcePath = "ClientApp";
 
